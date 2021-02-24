0e2204d4c9fd280276daea1971e2c8f9d5a9829c
Duvey <rudow.david@gmail.com>
2021-02-24 10:13:44 -0800

    added parallax effect

---
+++ Sisypean/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cam;
+    public float xOffset;
+    public float yOffset;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cam.transform.position + new Vector3(xOffset, yOffset, 10f);
+    }
+}
